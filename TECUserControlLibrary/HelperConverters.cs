8be1c0682a2277b6beb4aefc3a053e668c7df170
dtaylor1701 <dktaylor1701@gmail.com>
2017-01-19 16:22:01 -0500

    More edits to document builder

--- TECUserControlLibrary/HelperConverters.cs
+++ TECUserControlLibrary/HelperConverters.cs
@@ -255,4 +255,29 @@ namespace TECUserControlLibrary.HelperConverters
 
         #endregion
     }
+
+    public class CountVisibilityConverter : BaseConverter, IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if((int)value == 0)
+            {
+                return Visibility.Collapsed;
+            } else
+            {
+                return Visibility.Visible;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+
+
 }
