b27100eff3a5a3c871a92877e389bd8c18dc6399
Gregory <ghanson@TEC-system.com>
2017-03-01 15:18:01 -0500

    updated test bid

--- EstimatingUtilitiesLibrary/Properties/Settings.Designer.cs
+++ EstimatingUtilitiesLibrary/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace EstimatingUtilitiesLibrary.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1.3.0.16")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.3.0.17")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
