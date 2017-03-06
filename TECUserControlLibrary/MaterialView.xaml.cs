9f50f7d3c943d73a4b7a9904f0afd3a6c1e6459e
dtaylor1701 <dktaylor1701@gmail.com>
2017-03-06 16:06:11 -0500

    Added materials view and viewmodel

---
+++ TECUserControlLibrary/MaterialView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TECUserControlLibrary
+{
+    /// <summary>
+    /// Interaction logic for MaterialView.xaml
+    /// </summary>
+    public partial class MaterialView : UserControl
+    {
+        
+        public Object ViewModel
+        {
+            get { return (Object)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+            
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register("ViewModel", typeof(Object),
+                typeof(MaterialView));
+
+        public MaterialView()
+        {
+            InitializeComponent();
+        }
+    }
+}
