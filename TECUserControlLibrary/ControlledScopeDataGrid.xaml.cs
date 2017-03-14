cd546dd5407939834226be8f52c6589549e94a9d
David Taylor <dtaylor@tec-system.com>
2017-03-14 12:41:16 -0400

    Addec controlledScopeview

---
+++ TECUserControlLibrary/ControlledScopeDataGrid.xaml.cs
@@ -0,0 +1,81 @@
+﻿using EstimatingLibrary;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TECUserControlLibrary
+{
+    /// <summary>
+    /// Interaction logic for ControlledScopeDataGrid.xaml
+    /// </summary>
+    public partial class ControlledScopeDataGrid : UserControl
+    {
+        #region DPs
+
+        /// <summary>
+        /// Gets or sets the DevicesSource which is displayed
+        /// </summary>
+        public TECControlledScope SelectedControlledScope
+        {
+            get { return (TECControlledScope)GetValue(SelectedControlledScopeProperty); }
+            set { SetValue(ControlledScopeSourceProperty, value); }
+        }
+
+        /// <summary>
+        /// Identified the DevicesSource dependency property
+        /// </summary>
+        public static readonly DependencyProperty SelectedControlledScopeProperty =
+            DependencyProperty.Register("SelectedControlledScope", typeof(TECControlledScope),
+              typeof(ControlledScopeDataGrid), new PropertyMetadata(default(TECControlledScope)));
+
+        /// <summary>
+        /// Gets or sets the DevicesSource which is displayed
+        /// </summary>
+        public ObservableCollection<TECControlledScope> ControlledScopeSource
+        {
+            get { return (ObservableCollection<TECControlledScope>)GetValue(ControlledScopeSourceProperty); }
+            set { SetValue(ControlledScopeSourceProperty, value); }
+        }
+
+        /// <summary>
+        /// Identified the DevicesSource dependency property
+        /// </summary>
+        public static readonly DependencyProperty ControlledScopeSourceProperty =
+            DependencyProperty.Register("ControlledScopeSource", typeof(ObservableCollection<TECControlledScope>),
+              typeof(ControlledScopeDataGrid), new PropertyMetadata(default(ObservableCollection<TECControlledScope>)));
+
+        /// <summary>
+        /// Gets or sets the ViewModel which is used
+        /// </summary>
+        public Object ViewModel
+        {
+            get { return (Object)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+
+        /// <summary>
+        /// Identified the ViewModel dependency property
+        /// </summary>
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register("ViewModel", typeof(Object),
+              typeof(ControlledScopeDataGrid));
+
+        #endregion
+        public ControlledScopeDataGrid()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TECUserControlLibrary/ViewModels/ControlledScopeViewModel.cs
@@ -0,0 +1,31 @@
+﻿using EstimatingLibrary;
+using GalaSoft.MvvmLight;
+
+namespace TECUserControlLibrary.ViewModels
+{
+    /// <summary>
+    /// This class contains properties that a View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class ControlledScopeViewModel : ViewModelBase
+    {
+        private TECTemplates _templates;
+        public TECTemplates Templates
+        {
+            get { return _templates; }
+            set
+            {
+                _templates = value;
+                RaisePropertyChanged("Templates");
+            }
+        }
+
+        public ControlledScopeViewModel(TECTemplates templates)
+        {
+            Templates = templates;
+        }
+        
+    }
+}
