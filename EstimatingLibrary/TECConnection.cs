30e66a74d7a4f314192b7c7f324489baf57acd09
Gregory <ghanson@TEC-system.com>
2017-03-24 09:44:33 -0400

    Nerfed TECConnection

--- EstimatingLibrary/TECConnection.cs
+++ EstimatingLibrary/TECConnection.cs
@@ -9,14 +9,13 @@ using System.Threading.Tasks;
 namespace EstimatingLibrary
 {
 
-    public class TECConnection : TECObject
+    public abstract class TECConnection : TECObject
     {
         #region Properties
         private Guid _guid;
-        private double _length;
-        private TECController _controller;
-        private ObservableCollection<TECScope> _scope;
-        private ObservableCollection<IOType> _ioTypes;
+        protected double _length;
+        protected TECController _parentController;
+        protected TECConduitType _conduitType;
 
         public Guid Guid
         {
@@ -32,8 +31,7 @@ namespace EstimatingLibrary
                 NotifyPropertyChanged("Length", temp, this);
             }
         }
-        
-        public TECController Controller
+        public TECController ParentController
         {
             get { return _controller; }
             set
@@ -46,35 +44,6 @@ namespace EstimatingLibrary
                 NotifyPropertyChanged("RelationshipPropertyChanged", temp, oldNew);
             }
         }
-        public ObservableCollection<TECScope> Scope
-        {
-            get { return _scope; }
-            set
-            {
-                var temp = this.Copy();
-                Scope.CollectionChanged -= collectionChanged;
-                _scope = value;
-                NotifyPropertyChanged("Scope", temp, this);
-                Scope.CollectionChanged += collectionChanged;
-            }
-        }
-        public ObservableCollection<TECConnectionType> ConnectionTypes
-        {
-            get { return getConnectionTypes(); }
-            
-        }
-        public ObservableCollection<IOType> IOTypes
-        {
-            get { return _ioTypes; }
-            set
-            {
-                var temp = this.Copy();
-                _ioTypes = value;
-                NotifyPropertyChanged("IOTypes", temp, this);
-            }
-        }
-        
-        private TECConduitType _conduitType;
         public TECConduitType ConduitType
         {
             get { return _conduitType; }
@@ -95,11 +64,6 @@ namespace EstimatingLibrary
         {
             _guid = guid;
             _length = 0;
-            _scope = new ObservableCollection<TECScope>();
-            _ioTypes = new ObservableCollection<IOType>();
-            _controller = new TECController();
-            Scope.CollectionChanged += collectionChanged;
-            IOTypes.CollectionChanged += collectionChanged;
         }
 
         public TECConnection() : this(Guid.NewGuid()) { }
@@ -110,71 +74,10 @@ namespace EstimatingLibrary
             { guidDictionary[_guid] = connectionSource.Guid; }
 
             _length = connectionSource.Length;
-            foreach(TECScope scope in connectionSource.Scope)
-            {
-                if(scope is TECSubScope)
-                {
-                    _scope.Add(new TECSubScope((scope as TECSubScope), guidDictionary));
-                }
-                else if (scope is TECController)
-                {
-                    _scope.Add(new TECController((scope as TECController), guidDictionary, false));
-                }
-
-            }
-            _ioTypes = connectionSource.IOTypes;
-            _controller = new TECController(connectionSource.Controller, guidDictionary, false);
+            _parentController = new TECController(connectionSource.ParentController, guidDictionary, false);
             if (connectionSource.ConduitType != null)
             { _conduitType = connectionSource.ConduitType.Copy() as TECConduitType; }
         }
         #endregion //Constructors
-
-        #region Methods
-
-        private void collectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
-        {
-            if(e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
-            {
-                foreach(object item in e.NewItems)
-                {
-                    NotifyPropertyChanged("AddRelationship", this, item);
-                }
-            }
-            else if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Remove)
-            {
-                foreach (object item in e.OldItems)
-                {
-                    NotifyPropertyChanged("RemoveRelationship", this, item);
-                }
-            }
-        }
-
-        public override Object Copy()
-        {
-            TECConnection connection = new TECConnection(this);
-            connection._guid = this._guid;
-            return connection;
-        }
-
-        private ObservableCollection<TECConnectionType> getConnectionTypes()
-        {
-            var outConnectionTypes = new ObservableCollection<TECConnectionType>();
-
-            foreach(TECScope scope in Scope)
-            {
-                if(scope is TECSubScope)
-                {
-                    var sub = scope as TECSubScope;
-                    foreach(TECDevice dev in sub.Devices)
-                    {
-                        outConnectionTypes.Add(dev.ConnectionType);
-                    }
-                }
-            }
-            return outConnectionTypes;
-        }
-
-        #endregion
-
     }
 }
