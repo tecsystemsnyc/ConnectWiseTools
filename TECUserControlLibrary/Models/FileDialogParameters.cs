868084681903c57b81c5c62801b40c7ed0b9f59e
Gregory <ghanson@TEC-system.com>
2017-04-26 16:08:58 -0400

    Added FileParams to BuilderViewModel

---
+++ TECUserControlLibrary/Models/FileDialogParameters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TECUserControlLibrary.Models
+{
+    public struct FileDialogParameters
+    {
+        public string DefaultDirectory;
+        public string Filter;
+        public string DefaultExtension;
+    }
+}
