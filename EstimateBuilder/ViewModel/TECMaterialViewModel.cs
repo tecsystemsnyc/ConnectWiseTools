c1745435c687a7ba062e24251c04612118171e3c
Gregory <ghanson@TEC-system.com>
2017-05-15 17:02:36 -0400

    Added some stuff

--- EstimateBuilder/ViewModel/TECMaterialViewModel.cs
+++ EstimateBuilder/ViewModel/TECMaterialViewModel.cs
@@ -9,7 +9,12 @@ using System.ComponentModel;
 
 namespace EstimateBuilder.ViewModel
 {
-    public class DeviceSummaryViewModel : ViewModelBase
+    public enum TECMaterialIndex
+    {
+        Devices, Controllers, Panels, MiscCosts
+    }
+
+    public class TECMaterialViewModel : ViewModelBase
     {
         #region Properties
         private ChangeWatcher _changeWatcher;
@@ -156,7 +161,7 @@ namespace EstimateBuilder.ViewModel
         }
         #endregion
 
-        public DeviceSummaryViewModel(TECBid bid)
+        public TECMaterialViewModel(TECBid bid)
         {
             reinitialize(bid);
         }
--- TECUserControlLibrary/HelperConverters.cs
+++ TECUserControlLibrary/HelperConverters.cs
@@ -723,6 +723,22 @@ namespace TECUserControlLibrary.HelperConverters
         #endregion
     }
 
+    public class TECMaterialIndexToIntegerConverter : BaseConverter, IValueConverter
+    {
+        #region IValueConverter Members
 
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            int outInt = System.Convert.ToInt32(value);
+            return outInt;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (TECMaterialIndex)value;
+        }
+
+        #endregion
+    }
     #endregion
 }
