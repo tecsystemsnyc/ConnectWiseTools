9106ffc0881edc16f047ea6bdee8f80c12010fc1
Gregory <ghanson@TEC-system.com>
2017-05-24 10:05:29 -0400

    Changed ProposalView to use a combobox control

--- TECUserControlLibrary/HelperConverters.cs
+++ TECUserControlLibrary/HelperConverters.cs
@@ -680,5 +680,23 @@ namespace TECUserControlLibrary.HelperConverters
 
         #endregion
     }
+
+    public class ProposalIndexToIntegerConverter : BaseConverter, IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            int outInt = System.Convert.ToInt32(value);
+            return outInt;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (ProposalIndex)value;
+        }
+
+        #endregion
+    }
     #endregion
 }
