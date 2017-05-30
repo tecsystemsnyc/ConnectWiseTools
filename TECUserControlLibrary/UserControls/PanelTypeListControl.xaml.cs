803a060e88a41cb4a653a4fbd4bba26bc01093a0
Gregory <ghanson@TEC-system.com>
2017-05-30 11:35:32 -0400

    things

--- TECUserControlLibrary/UserControls/PanelTypeListControl.xaml.cs
+++ TECUserControlLibrary/UserControls/PanelTypeListControl.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace EstimateBuilder.View.Review
+namespace TECUserControlLibrary.UserControls
 {
     /// <summary>
     /// Interaction logic for PanelTypeListView.xaml
