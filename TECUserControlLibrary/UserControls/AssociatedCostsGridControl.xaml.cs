d9e0dfdab642b008548eeb96a2067870578164bc
dtaylor1701 <dktaylor1701@gmail.com>
2017-05-30 11:41:45 -0400

    Added EditVM dependency properties to editcontrols

--- TECUserControlLibrary/UserControls/AssociatedCostsGridControl.xaml.cs
+++ TECUserControlLibrary/UserControls/AssociatedCostsGridControl.xaml.cs
@@ -35,6 +35,22 @@ namespace TECUserControlLibrary.UserControls
               typeof(AssociatedCostsGridControl), new PropertyMetadata(default(ObservableCollection<TECCost>)));
 
 
+        /// <summary>
+        /// Gets or sets the ViewModel which is used
+        /// </summary>
+        public Object ViewModel
+        {
+            get { return (Object)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+
+        /// <summary>
+        /// Identified the ViewModel dependency property
+        /// </summary>
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register("ViewModel", typeof(Object),
+              typeof(AssociatedCostsGridControl));
+
         #endregion
         public AssociatedCostsGridControl()
         {
