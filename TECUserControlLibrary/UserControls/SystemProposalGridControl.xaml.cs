d642162a66e6670dc8e30f540cd6b3f551544027
Gregory <ghanson@TEC-system.com>
2017-05-31 11:21:54 -0400

    Added systemproposalgridcontrol

---
+++ TECUserControlLibrary/UserControls/SystemProposalGridControl.xaml.cs
@@ -0,0 +1,43 @@
+﻿using EstimatingLibrary;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TECUserControlLibrary.UserControls
+{
+    /// <summary>
+    /// Interaction logic for SystemProposalGridControl.xaml
+    /// </summary>
+    public partial class SystemProposalGridControl : UserControl
+    {
+        public ObservableCollection<TECSystem> SystemsSource
+        {
+            get { return (ObservableCollection<TECSystem>)GetValue(SystemsSourceProperty); }
+            set { SetValue(SystemsSourceProperty, value); }
+        }
+
+        /// <summary>
+        /// Identified the SystemSource dependency property
+        /// </summary>
+        public static readonly DependencyProperty SystemsSourceProperty =
+            DependencyProperty.Register("SystemsSource", typeof(ObservableCollection<TECSystem>),
+              typeof(SystemProposalGridControl), new PropertyMetadata(default(ObservableCollection<TECSystem>)));
+
+        public SystemProposalGridControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
