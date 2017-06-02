26de8cbe2029e2657875a8bcf25d598f463b53b4
dtaylor1701 <dktaylor1701@gmail.com>
2017-06-02 13:52:40 -0400

    Added instancesystem view and vm

---
+++ TECUserControlLibrary/ViewModels/InstanceSystemVM.cs
@@ -0,0 +1,20 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace TECUserControlLibrary.ViewModels
+{
+    /// <summary>
+    /// This class contains properties that a View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class InstanceSystemVM : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the InstanceSystemVM class.
+        /// </summary>
+        public InstanceSystemVM()
+        {
+        }
+    }
+}
