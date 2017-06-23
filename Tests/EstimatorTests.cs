4e0114f416320f6c90178c2a0b7b9438ab198577
Gregory <ghanson@TEC-system.com>
2017-06-23 09:50:35 -0400

    Fixed a test bug in Esimate_AddNetworkConneciton test

--- Tests/EstimatorTests.cs
+++ Tests/EstimatorTests.cs
@@ -483,8 +483,8 @@ namespace Tests
             connectionType.Labor = 1;
 
             var conduitType = new TECConduitType();
-            connectionType.Cost = 1;
-            connectionType.Labor = 1;
+            conduitType.Cost = 1;
+            conduitType.Labor = 1;
 
             var controller1 = new TECController(manufacturer);
             controller1.IO.Add(io);
