d8cd83bc00e7dcfa06191c7c416aa70857014de0
dtaylor1701 <dktaylor1701@gmail.com>
2017-07-12 13:13:59 -0400

    fixed add misc test

--- Tests/SaveStackTests.cs
+++ Tests/SaveStackTests.cs
@@ -690,11 +690,9 @@ namespace Tests
             typical.MiscCosts.Add(misc);
 
             List<StackItem> expectedItems = new List<StackItem>();
-            expectedItems.Add(new StackItem(Change.Add, misc, instance.MiscCosts[0], typeof(TECScope), typeof(TECScope)));
-            expectedItems.Add(new StackItem(Change.Add, instance, instance.MiscCosts[0]));
             expectedItems.Add(new StackItem(Change.Add, typical, misc));
 
-            int expectedCount = 1;
+            int expectedCount = expectedItems.Count;
 
 
             //Assert
