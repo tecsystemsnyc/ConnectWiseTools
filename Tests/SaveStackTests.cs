5b50523d5b418f70c39b5b98bf0b5415a984cd8e
dtaylor1701 <dktaylor1701@gmail.com>
2017-07-12 09:33:31 -0400

    Added more tests

--- Tests/SaveStackTests.cs
+++ Tests/SaveStackTests.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using EstimatingLibrary;
 using EstimatingUtilitiesLibrary;
+using System.Collections.ObjectModel;
 
 namespace Tests
 {
@@ -96,12 +97,10 @@ namespace Tests
             Assert.AreEqual(expectedCount, stack.SaveStack.Count);
             checkStackItems(expectedItems, stack);
         }
-       
 
-
-
+        #region SubScope
         [TestMethod]
-        public void Bid_AddSubScope()
+        public void Bid_AddSubScopeToTypicalWithout()
         {
             //Arrange
             TECBid bid = new TECBid();
@@ -122,7 +121,245 @@ namespace Tests
             Assert.AreEqual(expectedCount, stack.SaveStack.Count);
             checkStackItem(expectedItem, stack.SaveStack[stack.SaveStack.Count - 1]);
         }
-        
+
+        [TestMethod]
+        public void Bid_AddSubScopeToTypicalWith()
+        {
+            //Arrange
+            TECBid bid = new TECBid();
+            TECSystem system = new TECSystem();
+            bid.Systems.Add(system);
+            TECEquipment equipment = new TECEquipment();
+            system.Equipment.Add(equipment);
+            TECSystem instance = system.AddInstance(bid);
+
+            //Act
+            ChangeStack stack = new ChangeStack(bid);
+            TECSubScope subScope = new TECSubScope();
+            equipment.SubScope.Add(subScope);
+
+            List<StackItem> expectedItems = new List<StackItem>();
+            expectedItems.Add(new StackItem(Change.Add, subScope, instance.Equipment[0].SubScope[0], typeof(TECScope), typeof(TECScope)));
+            expectedItems.Add(new StackItem(Change.Add, instance.Equipment[0], instance.Equipment[0].SubScope[0]));
+            expectedItems.Add(new StackItem(Change.Add, system.Equipment[0], subScope));
+
+            int expectedCount = 3;
+
+            //Assert
+            Assert.AreEqual(expectedCount, stack.SaveStack.Count);
+            checkStackItems(expectedItems, stack);
+        }
+
+        [TestMethod]
+        public void Bid_AddInstanceToSystemWithSubScope()
+        {
+            //Arrange
+            TECBid bid = new TECBid();
+            TECSystem system = new TECSystem();
+            bid.Systems.Add(system);
+            TECEquipment equipment = new TECEquipment();
+            system.Equipment.Add(equipment);
+            TECSubScope subScope = new TECSubScope();
+            equipment.SubScope.Add(subScope);
+
+            //Act
+            ChangeStack stack = new ChangeStack(bid);
+            TECSystem instance = system.AddInstance(bid);
+            
+            List<StackItem> expectedItems = new List<StackItem>();
+            expectedItems.Add(new StackItem(Change.Add, subScope, instance.Equipment[0].SubScope[0], typeof(TECScope), typeof(TECScope)));
+            expectedItems.Add(new StackItem(Change.Add, instance.Equipment[0], instance.Equipment[0].SubScope[0]));
+            expectedItems.Add(new StackItem(Change.Add, equipment, instance.Equipment[0], typeof(TECScope), typeof(TECScope)));
+            expectedItems.Add(new StackItem(Change.Add, instance, instance.Equipment[0]));
+            expectedItems.Add(new StackItem(Change.Add, system, instance));
+
+            int expectedCount = 5;
+
+            //Assert
+            Assert.AreEqual(expectedCount, stack.SaveStack.Count);
+            checkStackItems(expectedItems, stack);
+        }
+        #endregion
+        #region Point
+        [TestMethod]
+        public void Bid_AddPointToTypicalWithout()
+        {
+            //Arrange
+            TECBid bid = new TECBid();
+            TECSystem system = new TECSystem();
+            bid.Systems.Add(system);
+            TECEquipment equipment = new TECEquipment();
+            system.Equipment.Add(equipment);
+            TECSubScope subScope = new TECSubScope();
+            equipment.SubScope.Add(subScope);
+
+            //Act
+            ChangeStack stack = new ChangeStack(bid);
+            TECPoint point = new TECPoint();
+            subScope.Points.Add(point);
+            
+            StackItem expectedItem = new StackItem(Change.Add, subScope, point);
+            int expectedCount = 1;
+
+            //Assert
+            Assert.AreEqual(expectedCount, stack.SaveStack.Count);
+            checkStackItem(expectedItem, stack.SaveStack[stack.SaveStack.Count - 1]);
+        }
+
+        [TestMethod]
+        public void Bid_AddPointToTypicalWith()
+        {
+            //Arrange
+            TECBid bid = new TECBid();
+            TECSystem system = new TECSystem();
+            bid.Systems.Add(system);
+            TECEquipment equipment = new TECEquipment();
+            system.Equipment.Add(equipment);
+            TECSubScope subScope = new TECSubScope();
+            equipment.SubScope.Add(subScope);
+            TECSystem instance = system.AddInstance(bid);
+
+            //Act
+            ChangeStack stack = new ChangeStack(bid);
+            TECPoint point = new TECPoint();
+            subScope.Points.Add(point);
+
+            List<StackItem> expectedItems = new List<StackItem>();
+            expectedItems.Add(new StackItem(Change.Add, point, instance.Equipment[0].SubScope[0].Points[0], typeof(TECScope), typeof(TECScope)));
+            expectedItems.Add(new StackItem(Change.Add, instance.Equipment[0].SubScope[0], instance.Equipment[0].SubScope[0].Points[0]));
+            expectedItems.Add(new StackItem(Change.Add, system.Equipment[0].SubScope[0], point));
+
+            int expectedCount = 3;
+
+            //Assert
+            Assert.AreEqual(expectedCount, stack.SaveStack.Count);
+            checkStackItems(expectedItems, stack);
+        }
+
+        [TestMethod]
+        public void Bid_AddInstanceToSystemWithPoint()
+        {
+            //Arrange
+            TECBid bid = new TECBid();
+            TECSystem system = new TECSystem();
+            bid.Systems.Add(system);
+            TECEquipment equipment = new TECEquipment();
+            system.Equipment.Add(equipment);
+            TECSubScope subScope = new TECSubScope();
+            equipment.SubScope.Add(subScope);
+            TECPoint point = new TECPoint();
+            subScope.Points.Add(point);
+
+            //Act
+            ChangeStack stack = new ChangeStack(bid);
+            TECSystem instance = system.AddInstance(bid);
+
+            List<StackItem> expectedItems = new List<StackItem>();
+            expectedItems.Add(new StackItem(Change.Add, point, instance.Equipment[0].SubScope[0].Points[0], typeof(TECScope), typeof(TECScope)));
+            expectedItems.Add(new StackItem(Change.Add, instance.Equipment[0].SubScope[0], instance.Equipment[0].SubScope[0].Points[0]));
+            expectedItems.Add(new StackItem(Change.Add, subScope, instance.Equipment[0].SubScope[0], typeof(TECScope), typeof(TECScope)));
+            expectedItems.Add(new StackItem(Change.Add, instance.Equipment[0], instance.Equipment[0].SubScope[0]));
+            expectedItems.Add(new StackItem(Change.Add, equipment, instance.Equipment[0], typeof(TECScope), typeof(TECScope)));
+            expectedItems.Add(new StackItem(Change.Add, instance, instance.Equipment[0]));
+            expectedItems.Add(new StackItem(Change.Add, system, instance));
+
+            int expectedCount = 7;
+
+            //Assert
+            Assert.AreEqual(expectedCount, stack.SaveStack.Count);
+            checkStackItems(expectedItems, stack);
+        }
+        #endregion
+        #region Device
+        [TestMethod]
+        public void Bid_AddDeviceToTypicalWithout()
+        {
+            //Arrange
+            TECBid bid = new TECBid();
+            TECSystem system = new TECSystem();
+            bid.Systems.Add(system);
+            TECEquipment equipment = new TECEquipment();
+            system.Equipment.Add(equipment);
+            TECSubScope subScope = new TECSubScope();
+            equipment.SubScope.Add(subScope);
+
+            //Act
+            ChangeStack stack = new ChangeStack(bid);
+            TECDevice device = new TECDevice(new ObservableCollection<TECConnectionType>(), new TECManufacturer());
+            subScope.Devices.Add(device);
+
+            StackItem expectedItem = new StackItem(Change.Add, subScope, device);
+            int expectedCount = 1;
+
+            //Assert
+            Assert.AreEqual(expectedCount, stack.SaveStack.Count);
+            checkStackItem(expectedItem, stack.SaveStack[stack.SaveStack.Count - 1]);
+        }
+
+        [TestMethod]
+        public void Bid_AddDeviceToTypicalWith()
+        {
+            //Arrange
+            TECBid bid = new TECBid();
+            TECSystem system = new TECSystem();
+            bid.Systems.Add(system);
+            TECEquipment equipment = new TECEquipment();
+            system.Equipment.Add(equipment);
+            TECSubScope subScope = new TECSubScope();
+            equipment.SubScope.Add(subScope);
+            TECSystem instance = system.AddInstance(bid);
+
+            //Act
+            ChangeStack stack = new ChangeStack(bid);
+            TECDevice device = new TECDevice(new ObservableCollection<TECConnectionType>(), new TECManufacturer());
+            subScope.Devices.Add(device);
+
+            List<StackItem> expectedItems = new List<StackItem>();
+            expectedItems.Add(new StackItem(Change.Add, device, instance.Equipment[0].SubScope[0].Devices[0], typeof(TECScope), typeof(TECScope)));
+            expectedItems.Add(new StackItem(Change.Add, instance.Equipment[0].SubScope[0], instance.Equipment[0].SubScope[0].Devices[0]));
+            expectedItems.Add(new StackItem(Change.Add, system.Equipment[0].SubScope[0], device));
+
+            int expectedCount = 3;
+
+            //Assert
+            Assert.AreEqual(expectedCount, stack.SaveStack.Count);
+            checkStackItems(expectedItems, stack);
+        }
+
+        [TestMethod]
+        public void Bid_AddInstanceToSystemWithDevice()
+        {
+            //Arrange
+            TECBid bid = new TECBid();
+            TECSystem system = new TECSystem();
+            bid.Systems.Add(system);
+            TECEquipment equipment = new TECEquipment();
+            system.Equipment.Add(equipment);
+            TECSubScope subScope = new TECSubScope();
+            equipment.SubScope.Add(subScope);
+            TECDevice device = new TECDevice(new ObservableCollection<TECConnectionType>(), new TECManufacturer());
+            subScope.Devices.Add(device);
+
+            //Act
+            ChangeStack stack = new ChangeStack(bid);
+            TECSystem instance = system.AddInstance(bid);
+
+            List<StackItem> expectedItems = new List<StackItem>();
+            expectedItems.Add(new StackItem(Change.Add, device, instance.Equipment[0].SubScope[0].Devices[0], typeof(TECScope), typeof(TECScope)));
+            expectedItems.Add(new StackItem(Change.Add, instance.Equipment[0].SubScope[0], instance.Equipment[0].SubScope[0].Devices[0]));
+            expectedItems.Add(new StackItem(Change.Add, subScope, instance.Equipment[0].SubScope[0], typeof(TECScope), typeof(TECScope)));
+            expectedItems.Add(new StackItem(Change.Add, instance.Equipment[0], instance.Equipment[0].SubScope[0]));
+            expectedItems.Add(new StackItem(Change.Add, equipment, instance.Equipment[0], typeof(TECScope), typeof(TECScope)));
+            expectedItems.Add(new StackItem(Change.Add, instance, instance.Equipment[0]));
+            expectedItems.Add(new StackItem(Change.Add, system, instance));
+
+            int expectedCount = 7;
+            
+            //Assert
+            Assert.AreEqual(expectedCount, stack.SaveStack.Count);
+            checkStackItems(expectedItems, stack);
+        }
+        #endregion
 
         public void checkStackItem(StackItem expectedItem, StackItem actualItem)
         {
