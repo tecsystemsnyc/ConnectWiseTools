ee654432dbaa10c9d00742b32b2b8096ca271014
Gregory <ghanson@TEC-system.com>
2017-09-13 13:20:27 -0400

    Fixed a test bug

--- EstimatingLibraryTests/ChangeWatcherTests.cs
+++ EstimatingLibraryTests/ChangeWatcherTests.cs
@@ -1406,8 +1406,7 @@ namespace Tests
 
             //Assert
             checkRaised(false, false, false);
-            checkInstanceChangedArgs(Change.Remove, "ChildrenConnections", controller, connection);
-            checkCostDelta(connection.CostBatch * -1);
+            checkChangedArgs(Change.Remove, "ChildrenConnections", controller, connection);
         }
 
         [TestMethod]
