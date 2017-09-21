64729bb776cdc26cb10d602d7e3a110396b54889
dtaylor1701 <dktaylor1701@gmail.com>
2017-09-21 11:32:24 -0400

    Added IO to iomodule

--- EstimatingLibrary/TECIOModule.cs
+++ EstimatingLibrary/TECIOModule.cs
@@ -1,6 +1,7 @@
 ﻿using EstimatingLibrary.Utilities;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,27 +12,48 @@ namespace EstimatingLibrary
     {
         private const CostType COST_TYPE = CostType.TEC;
 
-        private int _ioPerModule;
-        public int IOPerModule
+        private ObservableCollection<TECIO> _io;
+        public ObservableCollection<TECIO> IO
         {
-            get { return _ioPerModule; }
+            get { return _io; }
             set
             {
-                var old =  IOPerModule;
-                _ioPerModule = value;
-                notifyCombinedChanged(Change.Edit, "IOPerModule", this, value, old);
+                var old = IO;
+                IO.CollectionChanged -= (sender, args) => collectionChanged(sender, args, "IO");
+                _io = value;
+                IO.CollectionChanged += (sender, args) => collectionChanged(sender, args, "IO");
+                notifyCombinedChanged(Change.Edit, "IO", this, value, old);
             }
         }
         
         public TECIOModule(Guid guid, TECManufacturer manufacturer) : base(guid, manufacturer, COST_TYPE)
         {
-            _ioPerModule = 0;
+            _io = new ObservableCollection<TECIO>();
         }
         public TECIOModule(TECManufacturer manufacturer) : this(Guid.NewGuid(), manufacturer) { }
         public TECIOModule(TECIOModule ioModuleSource) : this(ioModuleSource.Manufacturer)
         {
             copyPropertiesFromHardware(this);
-            _ioPerModule = ioModuleSource.IOPerModule;
+            _io = ioModuleSource.IO;
         }
+
+        private void collectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e, string propertyName)
+        {
+            if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+            {
+                foreach (object item in e.NewItems)
+                {
+                    notifyCombinedChanged(Change.Add, propertyName, this, item);
+                }
+            }
+            else if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Remove)
+            {
+                foreach (object item in e.OldItems)
+                {
+                    notifyCombinedChanged(Change.Remove, propertyName, this, item);
+                }
+            }
+        }
+
     }
 }
