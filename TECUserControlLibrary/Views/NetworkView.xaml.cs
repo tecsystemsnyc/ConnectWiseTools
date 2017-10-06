c71ff04fef4666831e763c90c3856357f471292e
Gregory <ghanson@TEC-system.com>
2017-10-06 17:04:12 -0400

    Stuff

--- TECUserControlLibrary/Views/NetworkView.xaml.cs
+++ TECUserControlLibrary/Views/NetworkView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EstimatingLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -30,10 +31,70 @@ namespace TECUserControlLibrary.Views
         public static readonly DependencyProperty ViewModelProperty =
             DependencyProperty.Register("ViewModel", typeof(NetworkVM),
                 typeof(NetworkView));
+        
+        public double ConnectableWidth
+        {
+            get { return (double)GetValue(ConnectableWidthProperty); }
+            set
+            {
+                SetValue(ConnectableWidthProperty, value);
+                var width = ConnectableWidth;
+                Console.WriteLine("Value to set: " + value + " Actual: " + width);
+            }
+        }
+
+        // Using a DependencyProperty as the backing store for ChildrenWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ConnectableWidthProperty =
+            DependencyProperty.Register("ConnectableWidth", typeof(double), typeof(NetworkView), new PropertyMetadata(0.0));
+
+
+
+        public double HalfWidth
+        {
+            get { return (double)GetValue(HalfWidthProperty); }
+            set { SetValue(HalfWidthProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for HalfWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HalfWidthProperty =
+            DependencyProperty.Register("HalfWidth", typeof(double), typeof(NetworkView), new PropertyMetadata(0.0));
+
+
+
+        public TECNetworkConnection SelectedConnection
+        {
+            get { return (TECNetworkConnection)GetValue(SelectedConnectionProperty); }
+            set { SetValue(SelectedConnectionProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SelectedConnection.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectedConnectionProperty =
+            DependencyProperty.Register("SelectedConnection", typeof(TECNetworkConnection), typeof(NetworkView), new PropertyMetadata(default(TECNetworkConnection)));
+
+
+
+
+
 
         public NetworkView()
         {
             InitializeComponent();
+            ConnectionsBorder.SizeChanged += (sender, e) =>
+            {
+                if (e.WidthChanged)
+                {
+                    HalfWidth = e.NewSize.Width / 2;
+                    if (ConnectableWidth != 0)
+                    {
+                        ConnectableWidth = e.NewSize.Width/2;
+                    }
+                }
+            };
+        }
+
+        private void DoneButton_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedConnection = null;
         }
     }
 }
