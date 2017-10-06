67a5fdb7d535eba6306ae19bb3126a4c5fa40cc2
dtaylor1701 <dktaylor1701@gmail.com>
2017-10-06 15:20:42 -0400

    Added some cancel buttons

--- TECUserControlLibrary/Views/AddViews/AddBaseView.cs
+++ TECUserControlLibrary/Views/AddViews/AddBaseView.cs
@@ -20,7 +20,17 @@ namespace TECUserControlLibrary.Views.AddViews
             add { AddHandler(DoneEvent, value); }
             remove { RemoveHandler(DoneEvent, value); }
         }
-        
+
+        public static readonly RoutedEvent CancelEvent =
+        EventManager.RegisterRoutedEvent("Cancel", RoutingStrategy.Bubble,
+        typeof(RoutedEventHandler), typeof(AddBaseView));
+
+        public event RoutedEventHandler Cancel
+        {
+            add { AddHandler(CancelEvent, value); }
+            remove { RemoveHandler(CancelEvent, value); }
+        }
+
         public IAddVM ViewModel
         {
             get { return (IAddVM)GetValue(ViewModelProperty); }
@@ -33,6 +43,10 @@ namespace TECUserControlLibrary.Views.AddViews
         protected void doneButton_Click(object sender, RoutedEventArgs e)
         {
             RaiseEvent(new RoutedEventArgs(DoneEvent, this));
+        }
+        protected void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            RaiseEvent(new RoutedEventArgs(CancelEvent, this));
         }
     }
 }
