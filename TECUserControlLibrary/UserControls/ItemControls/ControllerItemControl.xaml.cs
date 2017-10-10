95518bf866a1913f979df4d46ca8fca62766d353
Gregory <ghanson@TEC-system.com>
2017-10-10 11:28:22 -0400

    Broke out controller item control

---
+++ TECUserControlLibrary/UserControls/ItemControls/ControllerItemControl.xaml.cs
@@ -0,0 +1,41 @@
+﻿using EstimatingLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TECUserControlLibrary.UserControls.ItemControls
+{
+    /// <summary>
+    /// Interaction logic for ControllerItemControl.xaml
+    /// </summary>
+    public partial class ControllerItemControl : UserControl
+    {
+
+
+        public TECController Controller
+        {
+            get { return (TECController)GetValue(ControllerProperty); }
+            set { SetValue(ControllerProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Controller.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ControllerProperty =
+            DependencyProperty.Register("Controller", typeof(TECController), typeof(ControllerItemControl));
+
+        public ControllerItemControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
