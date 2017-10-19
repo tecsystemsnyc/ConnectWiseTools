dd315b95915f592db0876bff2a22682220a98cd6
dtaylor1701 <dktaylor1701@gmail.com>
2017-10-19 14:49:56 -0400

    Added some views

---
+++ TECUserControlLibrary/ViewModels/SummaryVMs/PanelSummaryVM.cs
@@ -0,0 +1,18 @@
+﻿using EstimatingLibrary;
+using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TECUserControlLibrary.ViewModels.SummaryVMs
+{
+    public class PanelSummaryVM : ViewModelBase
+    {
+        public PanelSummaryVM(TECBid bid)
+        {
+
+        }
+    }
+}
