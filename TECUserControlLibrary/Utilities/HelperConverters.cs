12b469b06e9e7b330c58dcbe9d855315080873e8
dtaylor1701 <dktaylor1701@gmail.com>
2017-10-27 11:55:40 -0400

    forward button no longer displays after moving

--- TECUserControlLibrary/Utilities/HelperConverters.cs
+++ TECUserControlLibrary/Utilities/HelperConverters.cs
@@ -554,6 +554,30 @@ namespace TECUserControlLibrary.Utilities
         #endregion
     }
 
+    public class WidthDoubleToCollpasedConverter : BaseConverter, IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if ((double)value == 0.0)
+            {
+                return Visibility.Collapsed;
+            }
+            else
+            {
+                return Visibility.Visible;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+
     public class TypicalSelectionTemplateWidthConverter : BaseConverter, IValueConverter
     {
         #region IValueConverter Members
