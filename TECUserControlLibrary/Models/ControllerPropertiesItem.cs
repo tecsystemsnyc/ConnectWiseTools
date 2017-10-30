54fff45eeaa9fc2668fd47e1f315ef2e3105e7db
dtaylor1701 <dktaylor1701@gmail.com>
2017-10-30 15:56:23 -0400

    Added some preliminary controls stuff

---
+++ TECUserControlLibrary/Models/ControllerPropertiesItem.cs
@@ -0,0 +1,22 @@
+﻿using EstimatingLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TECUserControlLibrary.Models
+{
+    public class ControllerPropertiesItem
+    {
+        //public TECController Controller
+        //{
+
+        //}
+
+        public ControllerPropertiesItem(TECController controller)
+        {
+            
+        }
+    }
+}
---
+++ TECUserControlLibrary/UserControls/PropertyControls/ControllerPropertiesControl.xaml.cs
@@ -0,0 +1,41 @@
+﻿using EstimatingLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TECUserControlLibrary.UserControls.PropertyControls
+{
+    /// <summary>
+    /// Interaction logic for ControllerPropertiesControl.xaml
+    /// </summary>
+    public partial class ControllerPropertiesControl : UserControl
+    {
+
+        public TECController Selected
+        {
+            get { return (TECController)GetValue(SelectedProperty); }
+            set { SetValue(SelectedProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Selected.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectedProperty =
+            DependencyProperty.Register("Selected", typeof(TECController), typeof(ControllerPropertiesControl));
+
+
+        public ControllerPropertiesControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
