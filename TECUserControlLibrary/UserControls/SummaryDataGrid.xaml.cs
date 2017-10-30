b477e01c096c73166d703e89a1845b1ffe46bf1b
dtaylor1701 <dktaylor1701@gmail.com>
2017-10-30 16:13:28 -0400

    Made a thing

---
+++ TECUserControlLibrary/UserControls/SummaryDataGrid.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TECUserControlLibrary.UserControls
+{
+    /// <summary>
+    /// Interaction logic for SummaryDataGrid.xaml
+    /// </summary>
+    public partial class SummaryDataGrid : UserControl
+    {
+
+        public DataTemplate GridTemplate
+        {
+            get { return (DataTemplate)GetValue(GridTemplateProperty); }
+            set { SetValue(GridTemplateProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for GridTemplate.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty GridTemplateProperty =
+            DependencyProperty.Register("GridTemplate", typeof(DataTemplate), typeof(SummaryDataGrid));
+
+
+        public SummaryDataGrid()
+        {
+            InitializeComponent();
+        }
+    }
+}
