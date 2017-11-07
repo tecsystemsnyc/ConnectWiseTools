510533122bd43607664b024edc00a8704aaca385
dtaylor1701 <dktaylor1701@gmail.com>
2017-11-07 16:02:46 -0500

    Fixed a bunch of templates functionality

--- TECUserControlLibrary/Views/SplashView.xaml.cs
+++ TECUserControlLibrary/Views/SplashView.xaml.cs
@@ -25,6 +25,21 @@ namespace TECUserControlLibrary.Views
             DependencyProperty.Register("ViewModel", typeof(SplashVM),
               typeof(SplashView));
 
+
+
+        public Visibility BidVisibility
+        {
+            get { return (Visibility)GetValue(BidVisibilityProperty); }
+            set { SetValue(BidVisibilityProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for BidVisibility.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty BidVisibilityProperty =
+            DependencyProperty.Register("BidVisibility", typeof(Visibility), typeof(SplashView));
+
+
+
+
         public SplashView()
         {
             InitializeComponent();
