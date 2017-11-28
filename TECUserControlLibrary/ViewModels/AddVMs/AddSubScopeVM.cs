094fdde1ccdbd28ad0b90a240ac1a26325f86b5e
dtaylor1701 <dktaylor1701@gmail.com>
2017-11-28 16:42:11 -0500

    Added the ability to add points and devices upon subscope creation

--- TECUserControlLibrary/ViewModels/AddVMs/AddSubScopeVM.cs
+++ TECUserControlLibrary/ViewModels/AddVMs/AddSubScopeVM.cs
@@ -1,4 +1,5 @@
 ﻿using EstimatingLibrary;
+using EstimatingLibrary.Interfaces;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
 using System;
@@ -13,6 +14,9 @@ namespace TECUserControlLibrary.ViewModels.AddVMs
         private int quantity;
         private Action<TECSubScope> add;
         private bool isTypical = false;
+        private string _pointName;
+        private int _pointQuantity;
+        private IOType _pointType;
 
         public TECSubScope ToAdd
         {
@@ -32,29 +36,97 @@ namespace TECUserControlLibrary.ViewModels.AddVMs
                 RaisePropertyChanged("Quantity");
             }
         }
+        public string PointName
+        {
+            get { return _pointName; }
+            set
+            {
+                _pointName = value;
+                RaisePropertyChanged("PointName");
+            }
+        }
+        public int PointQuantity
+        {
+            get { return _pointQuantity; }
+            set
+            {
+                _pointQuantity = value;
+                RaisePropertyChanged("PointQuantity");
+            }
+        }
+        public IOType PointType
+        {
+            get { return _pointType; }
+            set
+            {
+                _pointType = value;
+                RaisePropertyChanged("PointType");
+            }
+        }
         public ICommand AddCommand { get; private set; }
+        public ICommand AddPointCommand { get; private set; }
+        public RelayCommand<IEndDevice> DeleteDeviceCommand { get; private set; }
+        public RelayCommand<TECPoint> DeletePointCommand { get; private set; }
 
         public AddSubScopeVM(TECEquipment parentEquipment)
         {
-            Quantity = 1;
             parent = parentEquipment;
             isTypical = parent.IsTypical;
             toAdd = new TECSubScope(parentEquipment.IsTypical);
-            AddCommand = new RelayCommand(addExecute, addCanExecute);
-
             add = subScope =>
             {
                 parent.SubScope.Add(subScope);
 
             };
+            setup();
         }
 
         public AddSubScopeVM(Action<TECSubScope> addMethod)
         {
             add = addMethod;
-            Quantity = 1;
             toAdd = new TECSubScope(false);
+            setup();
+        }
+        private void setup()
+        {
+            Quantity = 1;
+            PointQuantity = 1;
+            PointName = "";
+            PointType = IOType.AI;
             AddCommand = new RelayCommand(addExecute, addCanExecute);
+            AddPointCommand = new RelayCommand(addPointExecute, canAddPoint);
+            DeletePointCommand = new RelayCommand<TECPoint>(deletePointExecute);
+            DeleteDeviceCommand = new RelayCommand<IEndDevice>(deleteDeviceExecute);
+        }
+        
+        private void addPointExecute()
+        {
+            TECPoint newPoint = new TECPoint(isTypical);
+            newPoint.Type = PointType;
+            newPoint.Quantity = PointQuantity;
+            newPoint.Label = PointName;
+            ToAdd.Points.Add(newPoint);
+            PointName = "";
+        }
+        private bool canAddPoint()
+        {
+            if(PointQuantity != 0)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        private void deletePointExecute(TECPoint point)
+        {
+            toAdd.Points.Remove(point);
+        }
+
+        private void deleteDeviceExecute(IEndDevice device)
+        {
+            toAdd.Devices.Remove(device);
         }
 
         public Action<object> Added { get; }
