243cf660eedf93f037162f2e38e5452f7ee3dddf
Gregory <ghanson@TEC-system.com>
2017-12-01 14:46:29 -0500

    Added MaterialSummaryExport class

---
+++ EstimatingUtilitiesLibrary/Exports/MaterialSummaryExport.cs
@@ -0,0 +1,25 @@
+﻿using ClosedXML.Excel;
+using EstimatingLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstimatingUtilitiesLibrary.Exports
+{
+    public static class MaterialSummaryExport
+    {
+        public static void Generate(string path, TECBid bid)
+        {
+            XLWorkbook workbook = new XLWorkbook();
+        }
+
+        private static void addDevicesSheet(XLWorkbook workbook, TECBid bid)
+        {
+            const string sheetName = "Devices";
+
+            IXLWorksheet worksheet = workbook.Worksheets.Add(sheetName);
+        }
+    }
+}
