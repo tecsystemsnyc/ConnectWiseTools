3da7243110c3c0514c949459d43602b9800dda1d
Gregory <ghanson@TEC-system.com>
2017-12-07 10:40:06 -0500

    Added Scope Extension class

--- EstimatingLibrary/TECEquipment.cs
+++ EstimatingLibrary/TECEquipment.cs
@@ -64,7 +64,6 @@ namespace EstimatingLibrary
                 SubScope.Add(toAdd);
             }
             copyPropertiesFromScope(equipmentSource);
-            
         }
         #endregion //Constructors
 
---
+++ EstimatingLibrary/Utilities/ScopeExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstimatingLibrary.Utilities
+{
+    public static class ScopeExtensions
+    {
+        public static void CopyPropertiesFromScope(this TECScope scope, TECScope otherScope)
+        {
+            scope.Name = otherScope.Name;
+            scope.Description = otherScope.Description;
+            foreach (TECLabeled tag in otherScope.Tags)
+            {
+                scope.Tags.Add(tag);
+            }
+            foreach (TECCost cost in otherScope.AssociatedCosts)
+            {
+                scope.AssociatedCosts.Add(cost);
+            }
+        }
+    }
+}
