201e605212e2cc99ef550d4cb861db43fe265e97
dtaylor1701 <dktaylor1701@gmail.com>
2017-12-08 14:36:46 -0500

    Added a model for networkconnection

---
+++ TECUserControlLibrary/Models/NetworkConnectionVM.cs
@@ -0,0 +1,22 @@
+﻿using EstimatingLibrary;
+using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TECUserControlLibrary.Models
+{
+    public class NetworkConnectionVM : ViewModelBase
+    {
+        public TECNetworkConnection Connection { get; }
+        public List<TECElectricalMaterial> ConduitTypes { get; }
+
+        public NetworkConnectionVM(TECNetworkConnection connection, IEnumerable<TECElectricalMaterial> conduitTypes)
+        {
+            Connection = connection;
+            ConduitTypes = new List<TECElectricalMaterial>(conduitTypes);
+        }
+    }
+}
--- TECUserControlLibrary/UserControls/ListControls/NetworkConnectionListControl.xaml.cs
+++ TECUserControlLibrary/UserControls/ListControls/NetworkConnectionListControl.xaml.cs
@@ -1,4 +1,10 @@
 ﻿using EstimatingLibrary;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Data;
+using TECUserControlLibrary.Models;
+using TECUserControlLibrary.Utilities;
 
 namespace TECUserControlLibrary.UserControls.ListControls
 {
@@ -7,9 +13,47 @@ namespace TECUserControlLibrary.UserControls.ListControls
     /// </summary>
     public partial class NetworkConnectionListControl : BaseListControl<TECNetworkConnection>
     {
+
+
+        public IEnumerable<TECElectricalMaterial> ConduitTypes
+        {
+            get { return (IEnumerable<TECElectricalMaterial>)GetValue(ConduitTypesProperty); }
+            set { SetValue(ConduitTypesProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ConduitTypes.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ConduitTypesProperty =
+            DependencyProperty.Register("ConduitTypes", typeof(IEnumerable<TECElectricalMaterial>), typeof(NetworkConnectionListControl));
+
+
+
         public NetworkConnectionListControl()
         {
             InitializeComponent();
         }
+    }
+
+    public class NetworkConnectionToItemConverter : BaseConverter, IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value != null)
+            {
+                return new NetworkConnectionVM(value as TECNetworkConnection, parameter as IEnumerable<TECElectricalMaterial>);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+
+        }
+        #endregion
     }
 }
