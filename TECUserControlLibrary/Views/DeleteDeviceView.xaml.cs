f405a2b079a3f7d8ab2188425fb92e7ca9f975f2
dtaylor1701 <dktaylor1701@gmail.com>
2018-01-04 11:07:37 -0500

    Added some delete device infrastructure

---
+++ TECUserControlLibrary/Views/DeleteDeviceView.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TECUserControlLibrary.ViewModels;
+
+namespace TECUserControlLibrary.Views
+{
+    /// <summary>
+    /// Interaction logic for DeleteDeviceView.xaml
+    /// </summary>
+    public partial class DeleteDeviceView : UserControl
+    {
+
+
+        public DeleteDeviceVM ViewModel
+        {
+            get { return (DeleteDeviceVM)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ViewModel.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register("ViewModel", typeof(DeleteDeviceVM), typeof(DeleteDeviceView));
+
+
+        public DeleteDeviceView()
+        {
+            InitializeComponent();
+        }
+    }
+}
