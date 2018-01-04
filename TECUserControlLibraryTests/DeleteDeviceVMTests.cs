3e31b455ce3dfc6dacbc347c8e151261eb082bc4
Gregory Hanson <GHanson@tec-system.com>
2018-01-04 15:54:50 -0500

    Added DeleteDeviceVMTests

---
+++ TECUserControlLibraryTests/DeleteDeviceVMTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TECUserControlLibraryTests
+{
+    [TestClass]
+    public class DeleteDeviceVMTests
+    {
+        [TestMethod]
+        public void DeleteDevice()
+        {
+
+        }
+    }
+}
