1072f8cf8ba799a894c20e148a1ee712c9e90532
Gregory <ghanson@TEC-system.com>
2018-01-08 12:54:20 -0500

    Added DeleteDevice test

--- TECUserControlLibraryTests/DeleteDeviceVMTests.cs
+++ TECUserControlLibraryTests/DeleteDeviceVMTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TECUserControlLibrary.ViewModels;
 
 namespace TECUserControlLibraryTests
 {
@@ -23,7 +24,33 @@ namespace TECUserControlLibraryTests
             TECDevice dev = new TECDevice(new List<TECConnectionType>(), man);
             templates.Catalogs.Devices.Add(dev);
 
-            throw new NotImplementedException();
+            TECSystem sys = new TECSystem(false);
+            templates.SystemTemplates.Add(sys);
+            TECEquipment sysEquip = new TECEquipment(false);
+            sys.Equipment.Add(sysEquip);
+            TECSubScope sysSS = new TECSubScope(false);
+            sysEquip.SubScope.Add(sysSS);
+            sysSS.Devices.Add(dev);
+
+            TECEquipment equip = new TECEquipment(false);
+            templates.EquipmentTemplates.Add(equip);
+            TECSubScope equipSS = new TECSubScope(false);
+            equip.SubScope.Add(equipSS);
+            equipSS.Devices.Add(dev);
+
+            TECSubScope ss = new TECSubScope(false);
+            templates.SubScopeTemplates.Add(ss);
+            ss.Devices.Add(dev);
+
+            //Act
+            DeleteDeviceVM vm = new DeleteDeviceVM(dev, templates);
+            vm.DeleteCommand.Execute(null);
+
+            //Assert
+            Assert.IsFalse(templates.Catalogs.Devices.Contains(dev), "Device not removed from device templates properly.");
+            Assert.IsFalse(sysSS.Devices.Contains(dev), "Device not removed from system template properly.");
+            Assert.IsFalse(equipSS.Devices.Contains(dev), "Device not removed from equipment template properly.");
+            Assert.IsFalse(ss.Devices.Contains(dev), "Device not removed from subscope template properly.");
         }
 
         [TestMethod]
