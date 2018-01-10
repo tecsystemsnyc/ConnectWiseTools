2b5f8babc96916fb7d81e874db72c859247eb2fd
Gregory <ghanson@TEC-system.com>
2018-01-10 14:58:59 -0500

    Started Opportunities Export

--- ConnectWiseInformationInterface/Export/OpportunitiesExport.cs
+++ ConnectWiseInformationInterface/Export/OpportunitiesExport.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ClosedXML.Excel;
+using ConnectWiseDotNetSDK.ConnectWise.Client.Sales.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,47 @@ namespace ConnectWiseInformationInterface.Export
 {
     public static class OpportunitiesExport
     {
+        public static void ExportOpportunities(string filePath, IEnumerable<Opportunity> opps)
+        {
+            XLWorkbook workbook = new XLWorkbook();
+            IXLWorksheet worksheet = workbook.Worksheets.Add("SALES LOG");
 
+            worksheet.insertHeaders(1);
+
+            int i = 1;
+            foreach(Opportunity opp in opps)
+            {
+                IXLRow row = worksheet.Row(i);
+
+                double probability = opp.Probability;
+
+                row.Cell("A").Value = opp.
+
+                i++;
+            }
+        }
+
+        private static void insertHeaders(this IXLWorksheet ws, int row)
+        {
+            IXLRow headerRow = ws.Row(row);
+            headerRow.Style.Font.SetBold();
+
+            headerRow.Cell("A").Value = "Opportunity Number";
+            headerRow.Cell("B").Value = "Opportunity Name";
+            headerRow.Cell("C").Value = "Sales Person";
+            headerRow.Cell("D").Value = "Opportunity Type";
+            headerRow.Cell("E").Value = "Probability";
+            headerRow.Cell("F").Value = "Expected Close Date";
+            headerRow.Cell("G").Value = "Engineering Estimate";
+            headerRow.Cell("H").Value = "Engineering Forecast";
+            headerRow.Cell("I").Value = "Programming Estimate";
+            headerRow.Cell("J").Value = "Programming Forecast";
+            headerRow.Cell("K").Value = "Graphics Estimate";
+            headerRow.Cell("L").Value = "Graphics Forecast";
+            headerRow.Cell("M").Value = "Technician Estimate";
+            headerRow.Cell("N").Value = "Technician Forecast";
+            headerRow.Cell("O").Value = "PM Estimate";
+            headerRow.Cell("P").Value = "PM Forecast";
+        }
     }
 }
