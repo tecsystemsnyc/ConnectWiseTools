dbf23db78171830341a84971ff0c12c28aa7fbee
dtaylor1701 <dktaylor1701@gmail.com>
2018-01-16 09:24:23 -0500

    Futzed with itemized system summary

--- TECUserControlLibrary/Models/SystemSummaryItem.cs
+++ TECUserControlLibrary/Models/SystemSummaryItem.cs
@@ -7,15 +7,28 @@ namespace TECUserControlLibrary.Models
 {
     public class SystemSummaryItem : ViewModelBase
     {
+        private double _untiPrice;
+
         public TECTypical Typical { get; }
         public TECEstimator Estimate { get; }
-        public double UnitPrice { get; }
+        public double UnitPrice
+        {
+            get { return _untiPrice; }
+            set
+            {
+                _untiPrice = value;
+                RaisePropertyChanged("UnitPrice");
+            }
+        }
 
         public SystemSummaryItem(TECTypical typical, TECParameters parameters, double duration = 0.0)
         {
             this.Typical = typical;
             Estimate = new TECEstimator(Typical, parameters, new TECExtraLabor(Guid.NewGuid()), duration, new ChangeWatcher(Typical));
             UnitPrice = Estimate.TotalPrice / Typical.Instances.Count;
+            typical.CostChanged += change => {
+                UnitPrice = Estimate.TotalPrice / Typical.Instances.Count;
+            };
         }
         
     }
