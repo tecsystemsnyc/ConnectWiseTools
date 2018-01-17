5322e216c475e27136135444c57992c14a8f9553
Gregory <ghanson@TEC-system.com>
2018-01-17 11:43:40 -0500

    Plugged ConnectableFilter into NetworkVM

--- TECUserControlLibrary/ViewModels/ConnectableFilterVM.cs
+++ TECUserControlLibrary/ViewModels/ConnectableFilterVM.cs
@@ -1,12 +1,84 @@
-﻿using System;
+﻿using EstimatingLibrary.Interfaces;
+using GalaSoft.MvvmLight;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TECUserControlLibrary.ViewModels
 {
-    public class ConnectableFilterVM
+    public class ConnectableFilterVM<T> : ViewModelBase where T : INetworkConnectable
     {
+        private readonly ReadOnlyObservableCollection<T> allConnectables;
+
+        public ObservableCollection<T> FilteredConnectables { get; }
+
+        public ConnectableFilterVM(ObservableCollection<T> connectables)
+        {
+            allConnectables = new ReadOnlyObservableCollection<T>(connectables);
+            (allConnectables as INotifyCollectionChanged).CollectionChanged += allConnectablesCollectionChanged;
+            FilteredConnectables = new ObservableCollection<T>();
+            refilter();
+        }
+
+        private void allConnectablesCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                //Add any connectables that pass
+                foreach(object value in e.NewItems)
+                {
+                    if (value is T connectable && passesFilter(connectable))
+                    {
+                        FilteredConnectables.Add(connectable);
+                    } 
+                }
+            }
+            else if (e.Action == NotifyCollectionChangedAction.Remove)
+            {
+                //Remove any connectables that exist
+                foreach(object value in e.OldItems)
+                {
+                    if (value is T connectable && FilteredConnectables.Contains(connectable))
+                    {
+                        FilteredConnectables.Remove(connectable);
+                    }
+                }
+            }
+        }
+
+        private void refilter()
+        {
+            //Remove connectables that don't pass
+            List<T> toRemove = new List<T>();
+            foreach(T connectable in FilteredConnectables)
+            {
+                if (!passesFilter(connectable))
+                {
+                    toRemove.Add(connectable);
+                }
+            }
+            foreach(T connectable in toRemove)
+            {
+                FilteredConnectables.Remove(connectable);
+            }
+
+            //Add connectables that do pass
+            foreach(T connectable in allConnectables)
+            {
+                if (!FilteredConnectables.Contains(connectable) && passesFilter(connectable))
+                {
+                    FilteredConnectables.Add(connectable);
+                }
+            }
+        }
+
+        private bool passesFilter(T connectable)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- TECUserControlLibrary/ViewModels/NetworkVM.cs
+++ TECUserControlLibrary/ViewModels/NetworkVM.cs
@@ -19,40 +19,19 @@ namespace TECUserControlLibrary.ViewModels
     public class NetworkVM : ViewModelBase, IDropTarget
     {
         #region Fields and Properties
-        private readonly List<INetworkParentable> parentables;
-        private readonly List<INetworkConnectable> connectables;
-
-        private ObservableCollection<INetworkParentable> _filteredParentables;
-        private ObservableCollection<INetworkConnectable> _filteredConnectables;
+        private readonly ObservableCollection<INetworkParentable> allParentables;
+        private readonly ObservableCollection<INetworkConnectable> allConnectables;
 
         public ObservableCollection<INetworkParentable> FilteredParentables
         {
-            get { return _filteredParentables; }
-            set
-            {
-                if (FilteredParentables != value)
-                {
-                    _filteredParentables = value;
-                    RaisePropertyChanged("FilteredParentables");
-                }
-            }
+            get { return ParentableFilterVM.FilteredConnectables; }
         }
         public ObservableCollection<INetworkConnectable> FilteredConnectables
         {
-            get { return _filteredConnectables; }
-            set
-            {
-                if (FilteredConnectables != value)
-                {
-                    _filteredConnectables = value;
-                    RaisePropertyChanged("FilteredConnectables");
-                }
-            }
+            get { return ConnectableFilterVM.FilteredConnectables; }
         }
 
         private AddNetworkConnectionVM _addNetConnectVM;
-        private ConnectableFilterVM _parentableFilterVM;
-        private ConnectableFilterVM _connectableFilterVM;
 
         public AddNetworkConnectionVM AddNetConnectVM
         {
@@ -66,32 +45,13 @@ namespace TECUserControlLibrary.ViewModels
                 }
             }
         }
-        public ConnectableFilterVM ParentableFilterVM
+        public ConnectableFilterVM<INetworkParentable> ParentableFilterVM
         {
-            get { return _parentableFilterVM; }
-            set
-            {
-                if (ParentableFilterVM != value)
-                {
-                    _parentableFilterVM = value;
-                    RaisePropertyChanged("ParentableFilterVM");
-                }
-            }
+            get;
         }
-        public ConnectableFilterVM ConnectableFilterVM
+        public ConnectableFilterVM<INetworkConnectable> ConnectableFilterVM
         {
-            get
-            {
-                return _connectableFilterVM;
-            }
-            set
-            {
-                if (ConnectableFilterVM != value)
-                {
-                    _connectableFilterVM = value;
-                    RaisePropertyChanged("ConnectableFilterVM");
-                }
-            }
+            get;
         }
         #endregion
 
@@ -101,15 +61,17 @@ namespace TECUserControlLibrary.ViewModels
             Action<TECController> updateExecute = null,
             Func<TECController, bool> updateCanExecute = null)
         {
-            this.connectables = new List<INetworkConnectable>(connectables);
-            this.parentables = new List<INetworkParentable>();
+            this.allParentables = new ObservableCollection<INetworkParentable>();
+            this.allConnectables = new ObservableCollection<INetworkConnectable>(connectables);
             foreach(INetworkConnectable connectable in connectables)
             {
                 if (connectable is INetworkParentable parentable)
                 {
-                    parentables.Add(parentable);
+                    allParentables.Add(parentable);
                 }
             }
+            ParentableFilterVM = new ConnectableFilterVM<INetworkParentable>(allParentables);
+            ConnectableFilterVM = new ConnectableFilterVM<INetworkConnectable>(allConnectables);
         }
 
         public void DragOver(IDropInfo dropInfo)
