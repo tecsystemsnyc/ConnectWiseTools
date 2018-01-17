76e3dd4208baef0f5db1d1b98d9a75e0ce597222
dtaylor1701 <dktaylor1701@gmail.com>
2018-01-17 11:04:47 -0500

    Added some more networkview

--- TECUserControlLibrary/Views/NetworkView.xaml.cs
+++ TECUserControlLibrary/Views/NetworkView.xaml.cs
@@ -22,54 +22,10 @@ namespace TECUserControlLibrary.Views
             DependencyProperty.Register("ViewModel", typeof(NetworkVM),
                 typeof(NetworkView));
         
-        public double ConnectableWidth
-        {
-            get { return (double)GetValue(ConnectableWidthProperty); }
-            set
-            {
-                SetValue(ConnectableWidthProperty, value);
-                var width = ConnectableWidth;
-            }
-        }
-
-        // Using a DependencyProperty as the backing store for ChildrenWidth.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty ConnectableWidthProperty =
-            DependencyProperty.Register("ConnectableWidth", typeof(double), typeof(NetworkView), new PropertyMetadata(0.0));
-
-        public double HalfWidth
-        {
-            get { return (double)GetValue(HalfWidthProperty); }
-            set { SetValue(HalfWidthProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for HalfWidth.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty HalfWidthProperty =
-            DependencyProperty.Register("HalfWidth", typeof(double), typeof(NetworkView), new PropertyMetadata(0.0));
-
+        
         public NetworkView()
         {
             InitializeComponent();
-            ConnectionsBorder.SizeChanged += (sender, e) =>
-            {
-                if (e.WidthChanged)
-                {
-                    HalfWidth = e.NewSize.Width / 2;
-                    if (ConnectableWidth != 0)
-                    {
-                        ConnectableWidth = e.NewSize.Width/2;
-                    }
-                }
-            };
-        }
-
-        private void NetworkConnectionListControl_Selected(object sender, RoutedEventArgs e)
-        {
-            var selectedItem = (sender as NetworkConnectionListControl).SelectedItem;
-            if (selectedItem != null)
-            {
-                DoubleAnimation showChildren = new DoubleAnimation(0, HalfWidth, TimeSpan.FromMilliseconds(200));
-                BeginAnimation(ConnectableWidthProperty, showChildren);
-            }
         }
     }
 }
